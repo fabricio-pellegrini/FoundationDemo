6f6b3b196213ff9714507499116462404bd71440
Fabricio Pellegrini <fabricio.pellegrini@gmail.com>
2012-11-24 16:35:27 -0300

    #Start

---
+++ FoundationDemo/Controllers/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoundationDemo.Controllers
+{
+    public class SettingsController : Controller
+    {
+        //
+        // GET: /Settings/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+    }
+}
